53e2d0f864fde38bd960f2e6dab98324ce8b5977
TwelveTwenty1220 <280275907@qq.com>
2023-07-21 12:22:30 +0800

    Prototype 2

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float pressInterval = 1.0f;
+    private int flag = 0;
+
+    private void Start()
+    {
+        InvokeRepeating("EnablePress", 0, pressInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (flag == 1)
+        {
+           
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                flag = 0;
+            }
+
+        }
+    }
+    void EnablePress()
+    {
+        flag = 1;
+    }
+}
